8d02b10d2ee67666630cd4ea31d6563a3c7d0259
Nathan Sarrazin <sarrazin.nathan@gmail.com>
2025-08-26 18:04:32 +0200

    feat: basic og web stuff

--- web/src/routes/+layout.ts
+++ web/src/routes/+layout.ts
@@ -1,1 +1,15 @@
-export const ssr = false;
+// This can be used to set page-specific meta data in the future
+export const prerender = true;
+
+// Default SEO configuration that can be overridden by individual pages
+export const load = () => {
+	return {
+		meta: {
+			title: 'SpessComputer - 6502 Assembly Space Computer Simulator',
+			description: 'Program a retro 6502-based space computer to control spacecraft in this interactive programming simulator. Write assembly code, manage memory, and navigate through space.',
+			url: '',
+			image: '/SpessComputer-og.png',
+			type: 'website'
+		}
+	};
+};
